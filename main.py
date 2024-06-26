1f25a223f5a256a791ae48fd8bc0b0e1348fb05a
TimusVladimir <timusvladimir@gmail.com>
2024-06-26 09:55:32 +0300

    Make board 3

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 WIDTH, HEIGHT = 8, 8
 
+
 def newBoard():
     board = []
     for i in range(WIDTH):
         board.append(['', '', '', '', '', '', '', ''])
     return board
 
+
 def pictureBoard(board):
-    print(' 12345678')
-    print('+--------+')
+    print('  12345678')
+    print(' +--------+')
     for y in range(HEIGHT):
         print('%s|' % (y+1), end='')
         for x in range(WIDTH):
             print(board[x][y], end='')
         print('|%s' % (y+1))
-    print('+--------+')
-    print(' 12345678')
+    print(' +--------+')
+    print('  12345678')
 
 
 print('Это игра Реверси')
